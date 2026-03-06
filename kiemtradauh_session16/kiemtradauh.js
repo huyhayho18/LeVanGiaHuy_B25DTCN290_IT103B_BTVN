a47bcbcf6dcb210c3227a727b335fbe137a14e88
huyhayho18 <giahuylevan1807@gmail.com>
2026-03-06 15:56:33 +0700

    Add files via upload

---
+++ kiemtradauh_session16/kiemtradauh.js
@@ -0,0 +1,27 @@
+let listTask = [
+    {name: 'nấu cơm' },
+    {name: 'rửa rau'},
+    {name: 'dọn nhà'}
+]
+
+let inputTask = document.getElementById(`taskInput`);
+let addBtn = document.getElementById(`addBtn`);
+let taskLisk = document.getElementById('taskList');
+
+addBtn.addEventListener(`click`,create)
+
+function create() {
+    let newList = {
+        name: inputTask.value
+    }
+    if(inputTask ==='') {
+        alert('Vui lòng nhập tên công việc!');
+        return;
+    }
+    listTask.push(newList);
+    taskLisk.innerHTML= `
+        <li>${newList.name}</li>
+    `
+    inputTask.innerHTML ='';
+}
+console.log(listTask)
